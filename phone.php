76b8dfefb53c9e81447a66343d9dea134fe9966e
G7-Hckr <48849606+G7-Hckr@users.noreply.github.com>
2019-03-25 21:47:06 +0700

    Update phone.php

--- phone.php
+++ phone.php
@@ -2,12 +2,12 @@
 // Limit 3x Telpon Setiap Satu Nomor
 function send($phone){
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth                                                        >
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=ca                                                        >
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
         curl_close($ch);
                 echo $asw."\n";
 }
